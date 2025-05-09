8c5492b845f575a0ce49dbe46c728ab99f860d72
Andamlaka <andargachweewawey@gmail.com>
2025-05-09 16:19:49 +0300

    footer done

--- netflix-clone/src/components/Footer/Footer.js
+++ netflix-clone/src/components/Footer/Footer.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div>
+        <ul className="social-icons-list">
+          <li><FacebookOutlinedIcon/></li>
+          <li><InstagramIcon/></li>
+          <li><YouTubeIcon/></li>
+          
+        </ul>
+      </div>
 
-export default Footer
+      <div className="footer-links-container">
+        <ul className="footer-column">
+          <li><a href="#">Audio Description</a></li>
+          <li><a href="#">Investor Relations</a></li>
+          <li><a href="#">Legal Notice</a></li>
+          <li><button className="service-code">Service Code</button></li>
+        </ul>
+        <ul className="footer-column">
+          <li><a href="#">Help Center</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Cookie Preferences</a></li>
+        </ul>
+        <ul className="footer-column">
+          <li><a href="#">Gift Cards</a></li>
+          <li><a href="#">Terms of Use</a></li>
+          <li><a href="#">Corporate Information</a></li>
+        </ul>
+        <ul className="footer-column">
+          <li><a href="#">Media Center</a></li>
+          <li><a href="#">Privacy</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+
+      <div className="footer-bottom-text">
+        &copy; 1997–2025 Netflix, Inc.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- netflix-clone/src/components/Header/Header.js
+++ netflix-clone/src/components/Header/Header.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './header.css'
 import NetflixLogo from '../../assets/images/NetflixLogo.png'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
 const Header = () => {
   return (
     <div className="header_outer_container">
@@ -19,10 +24,11 @@ const Header = () => {
       <div className="header-right">
 
         <ul className="nav-list">
-           {/* <li><SearchIcon/></li> 
-          <li className="nav-item">NotificationIcon</li>
-          <li className="nav-item">AcountBoxIcon</li>
-          <li className="nav-item">ArrowDropdownIcon</li> */}
+          <li><SearchIcon/></li> 
+          <li><NotificationsNoneIcon/></li>
+          <li><AccountBoxIcon/></li>
+          <li><ArrowDropDownIcon/></li>
+         
         </ul>
       </div>
     </div>
