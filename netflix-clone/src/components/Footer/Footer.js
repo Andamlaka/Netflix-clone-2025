270fd8f2302357dd43c658642deeda828edcfdb6
Andamlaka <andargachweewawey@gmail.com>
2025-05-11 09:42:50 +0300

    inhancing footer for teh anchor tag

--- netflix-clone/src/components/Footer/Footer.js
+++ netflix-clone/src/components/Footer/Footer.js
@@ -9,34 +9,33 @@ const Footer = () => {
     <footer className="footer">
       <div>
         <ul className="social-icons-list">
-          <li><FacebookOutlinedIcon/></li>
-          <li><InstagramIcon/></li>
-          <li><YouTubeIcon/></li>
-          
+          <li><FacebookOutlinedIcon /></li>
+          <li><InstagramIcon /></li>
+          <li><YouTubeIcon /></li>
         </ul>
       </div>
 
       <div className="footer-links-container">
         <ul className="footer-column">
-          <li><a href="#">Audio Description</a></li>
-          <li><a href="#">Investor Relations</a></li>
-          <li><a href="#">Legal Notice</a></li>
+          <li><a href="/audio-description">Audio Description</a></li>
+          <li><a href="/investor-relations">Investor Relations</a></li>
+          <li><a href="/legal-notice">Legal Notice</a></li>
           <li><button className="service-code">Service Code</button></li>
         </ul>
         <ul className="footer-column">
-          <li><a href="#">Help Center</a></li>
-          <li><a href="#">Jobs</a></li>
-          <li><a href="#">Cookie Preferences</a></li>
+          <li><a href="/help-center">Help Center</a></li>
+          <li><a href="/jobs">Jobs</a></li>
+          <li><a href="/cookie-preferences">Cookie Preferences</a></li>
         </ul>
         <ul className="footer-column">
-          <li><a href="#">Gift Cards</a></li>
-          <li><a href="#">Terms of Use</a></li>
-          <li><a href="#">Corporate Information</a></li>
+          <li><a href="/gift-cards">Gift Cards</a></li>
+          <li><a href="/terms-of-use">Terms of Use</a></li>
+          <li><a href="/corporate-info">Corporate Information</a></li>
         </ul>
         <ul className="footer-column">
-          <li><a href="#">Media Center</a></li>
-          <li><a href="#">Privacy</a></li>
-          <li><a href="#">Contact Us</a></li>
+          <li><a href="/media-center">Media Center</a></li>
+          <li><a href="/privacy">Privacy</a></li>
+          <li><a href="/contact-us">Contact Us</a></li>
         </ul>
       </div>
 
